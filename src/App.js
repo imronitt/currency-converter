8ec8c0e968507f976bb45dc798d262396444a202
Ronit Bhattacharjee <a00268799@student.ait.ie>
2021-02-12 02:14:48 +0530

    V6

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Converter from "./component/converter";
-
+import CurrencyRate from "./component/CurrencyRate";
 import { 
 	BrowserRouter as Router, 
 	Route, 
 	Link, 
 	Switch 
 } from 'react-router-dom'; 
-import CurrencyRate from "./component/CurrencyRate";
+
 
 
 
@@ -24,12 +24,12 @@ return (
        
       <div class="topnav">
         <a class="active" href="/">Currency Converter</a>
-        <a href="/CurrencyRate">Current Exchange Rate</a>
+        <a href="/rate">Current Exchange Rate</a>
 
       </div>
        <Switch> 
          <Route exact path='/' component={Converter}></Route> 
-         <Route exact path='/CurrencyRate' component={CurrencyRate}></Route> 
+         <Route exact path='/rate' component={CurrencyRate}></Route> 
        </Switch> 
      </div> 
   </Router> 
